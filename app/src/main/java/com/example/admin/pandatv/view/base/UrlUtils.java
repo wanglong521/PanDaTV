af6de6aecf78c8e7655ca769e3b1e093e54dcac3
Administrator <617713752@qq.com>
2017-08-25 08:14:14 +0800

    上传接口

---
+++ app/src/main/java/com/example/admin/pandatv/view/base/UrlUtils.java
@@ -0,0 +1,67 @@
+package com.example.admin.pandatv.view.base;
+
+/**
+ * Created by lenovo on 2017/8/24.
+ */
+
+public class UrlUtils {
+    //服务器地址
+    public static final String BASEURL = "http://www.ipanda.com/kehuduan/";
+    //首页
+    public static final String HOMEURLALL = "http://www.ipanda.com/kehuduan/shouye/index.json";
+    //直播中国
+    public static final String LIVECHINAURL = "http://www.ipanda.com/kehuduan/PAGE14501775094142282/index.json";
+    //熊猫播报
+    public static final String PANDAREPORT = "http://api.cntv.cn/apicommon/index";
+    // 熊猫直播
+    public static final String PANDALIVE = "http://www.ipanda.com/kehuduan/PAGE14501769230331752/index.json";
+    //多角度
+    public static final String MULITANGLE = "http://www.ipanda.com/kehuduan/PAGE14501769230331752/PAGE14501787896813312/index.json";
+    //首页视频
+    public static final String HOMEVIDEOURL = "http://www.ipanda.com/kehuduan/shipinliebieye/xiongmaoguancha/index.json";
+    //首页-原创互动
+    public static final String ORIGINAL = "http://www.ipanda.com/kehuduan/PAGE14501767715521482/index.json";
+
+    public static final String BADALING = "http://www.ipanda.com/kehuduan/liebiao/badaling/index.json";
+
+    public static final String DANGXIONG = "http://api.cntv.cn/video/videolistById?vsid=VSET100332640004&n=7&serviceId=panda&o=desc&of=time&p=1";
+    //超萌滚滚秀
+    public static final String CHAOMENG = "http://api.cntv.cn/video/videolistById?vsid=VSET100272959126&n=7&serviceId=panda&o=desc&of=time&p=1";
+    //滚滚视频
+    public static final String ROLLVIDEO = "http://www.ipanda.com/kehuduan/video/index.json";
+    //熊猫播报2                              //http://www.ipanda.com/kehuduan/news/index.json
+    public static final String PANDAREPORTTWO = "http://api.cntv.cn/apicommon/index?path=iphoneInterface/general/getArticleAndVideoListInfo.json&primary_id=PAGE1422435191506336&serviceId=panda";
+    //熊猫top榜
+    public static final String PANDATOPNOTIVE = "http://115.182.35.91/api/getVideoInfoForCBox.do?pid=3d92fae34dc14b2492de15d5dd122ac8";
+    //熊猫top榜条目1
+    public static final String PANDATOPNOTIVEITEM1 = "http://api.cntv.cn/video/videolistById?n=10&vsid=VSET100284428835&p=1&serviceId=panda&em=1";
+    //熊猫档案
+    public static final String DANGAN = "http://api.cntv.cn/video/videolistById?vsid=VSET100340574858&n=7&serviceId=panda&o=desc&of=time&p=1";
+    //熊猫TOP榜
+    public static final String TOPBANG = "http://api.cntv.cn/video/videolistById?vsid=VSET100284428835&n=7&serviceId=panda&o=desc&of=time&p=1";
+    //熊猫那些事
+    public static final String NAXIESHI = "http://api.cntv.cn/video/videolistById?vsid=VSET100237714751&n=7&serviceId=panda&o=desc&of=time&p=1";
+    //特别节目
+    public static final String TEBIE = "http://api.cntv.cn/video/videolistById?vsid=VSET100167308855&n=7&serviceId=panda&o=desc&of=time&p=1";
+    //原创新闻
+    public static final String YUANCHUANG = "http://api.cntv.cn/video/videolistById?vsid=VSET100219009515&n=7&serviceId=panda&o=desc&of=time&p=1";
+
+    // 首页
+    public static final String HOME_WONDERFUL = "http://www.ipanda.com/kehuduan/shipinliebieye/jingcaiyike/index.json";
+
+    //精彩一刻
+    public static final String ORIGINALNEWS = "http://api.cntv.cn/video/videolistById?vsid=VSET100167216881&n=7&serviceId=panda&o=desc&of=time&p=1";
+
+    // 首页  滚滚视频
+    public static final String HOME_ROLL = "http://www.ipanda.com/kehuduan/shipinliebieye/video/index.json";
+
+    //图形验证码
+    public static final String IMAGE_YZM = "http://reg.cntv.cn/simple/verificationCode.action";
+    //手机验证码
+    public static final String PHONE_URL = "http://reg.cntv.cn/regist/getVerifiCode.action";
+    //登录
+    public static final String LOGIN = "https://reg.cntv.cn/login/login.action";
+    //个人中心
+    public static final String PHONE_URL_FROM = "http://cbox_mobile.regclientuser.cntv.cn";
+
+}
