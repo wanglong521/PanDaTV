1aa8d1b6e2f89688492a4384d60e5464c3b9bcb0
Administrator <617713752@qq.com>
2017-08-26 15:22:58 +0800

    熊猫播报完善

--- app/src/main/java/com/example/admin/pandatv/view/activity/MainActivity.java
+++ app/src/main/java/com/example/admin/pandatv/view/activity/MainActivity.java
@@ -25,9 +25,9 @@ import static com.example.admin.pandatv.R.id.mHomeModule;
 import static com.example.admin.pandatv.R.id.mLiveModule;
 import static com.example.admin.pandatv.R.id.mRollingModule;
 
-public class MainActivity extends BaseActivity implements IView{
+public class MainActivity extends BaseActivity implements IView {
     private android.support.v4.app.FragmentManager supportFragmentManager;
-    RadioGroup   HomeRadioGroup;
+    RadioGroup HomeRadioGroup;
     private double mExitTime;
 
     @Override
@@ -84,13 +84,13 @@ public class MainActivity extends BaseActivity implements IView{
         /**
          * 用来添加Fragment
          */
-    supportFragmentManager= App.mBaseActivity.getSupportFragmentManager();
+        supportFragmentManager = App.mBaseActivity.getSupportFragmentManager();
         FragmentBuilder.getInstance().init().initContainId(R.id.homeframe).add(HomeModule_Fragment.class).build();
     }
 
     @Override
     protected void initView() {
-        HomeRadioGroup=   (RadioGroup) findViewById(R.id.HomeRadioGroup);
+        HomeRadioGroup = (RadioGroup) findViewById(R.id.HomeRadioGroup);
 
     }
 
@@ -100,11 +100,8 @@ public class MainActivity extends BaseActivity implements IView{
     }
 
     @Override
-<<<<<<< HEAD
-    public void OnSucceed(String succed) {
-=======
     public void OnSucceed(BroadcastBean succed) {
->>>>>>> origin/master
+
 
     }
 
@@ -128,15 +125,15 @@ public class MainActivity extends BaseActivity implements IView{
      * 获取栈顶的fragment的名字，判断名字是否和主页的名字是否一样，
      * 如果一样就退出应用，如果不是就回退上一个fragment；
      */
-   @Override
+    @Override
     public void onBackPressed() {
         //通过FragmentManger来获取回退栈中位于栈顶的Fregment的名字
 
 
         String simpleName = supportFragmentManager.getBackStackEntryAt(supportFragmentManager.getBackStackEntryCount() - 1).getName();
-        if ("HomeModule_Fragment".equals(simpleName)|"LiveModule_Fragment".equals(simpleName)
-                |"RollingModule_Fragment".equals(simpleName)|"BroadcastModule_Fragment".equals(simpleName)
-                |"ChinaModule_Fragment".equals(simpleName)) {
+        if ("HomeModule_Fragment".equals(simpleName) | "LiveModule_Fragment".equals(simpleName)
+                | "RollingModule_Fragment".equals(simpleName) | "BroadcastModule_Fragment".equals(simpleName)
+                | "ChinaModule_Fragment".equals(simpleName)) {
             finish();
         } else {
             if (supportFragmentManager.getBackStackEntryCount() > 1) {
@@ -151,10 +148,10 @@ public class MainActivity extends BaseActivity implements IView{
     public boolean onKeyDown(int keyCode, KeyEvent event) {
         //通过FragmentManger来获取回退栈中位于栈顶的Fregment的名字
         //supportFragmentManager.getBackStackEntryCount()  该方法获取fragment栈里面Fragment的总数
-        String  simpleName = supportFragmentManager.getBackStackEntryAt(supportFragmentManager.getBackStackEntryCount() - 1).getName();
+        String simpleName = supportFragmentManager.getBackStackEntryAt(supportFragmentManager.getBackStackEntryCount() - 1).getName();
 
-        if ("HomeModule_Fragment".equals(simpleName)|"LiveModule_Fragment".equals(simpleName)|"RollingModule_Fragment".equals(simpleName)
-                |"BroadcastModule_Fragment".equals(simpleName)|"ChinaModule_Fragment".equals(simpleName)) {
+        if ("HomeModule_Fragment".equals(simpleName) | "LiveModule_Fragment".equals(simpleName) | "RollingModule_Fragment".equals(simpleName)
+                | "BroadcastModule_Fragment".equals(simpleName) | "ChinaModule_Fragment".equals(simpleName)) {
             if (keyCode == KeyEvent.KEYCODE_BACK) {//back键被按下了
                 if ((System.currentTimeMillis() - mExitTime) > 2000) {//第二次点击判断是否在两秒内完成，是的话Finish掉（退出）
                     Toast.makeText(this, "连续点击两次退出程序", Toast.LENGTH_SHORT).show();
