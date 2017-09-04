a1ca81c839a024505bb99295b22469230745bbb7
lenovo <793908669@qq.com>
2017-09-04 17:00:52 +0800

    侯东明第5次上传的项目

---
+++ app/src/main/java/com/example/admin/pandatv/view/adapter/callbackimpl/Enabledimpl.java
@@ -0,0 +1,9 @@
+package com.example.admin.pandatv.view.adapter.callbackimpl;
+
+/**
+ * Created by lenovo on 2017/9/4.
+ */
+
+public interface Enabledimpl {
+    void getEbabked(Boolean aBoolean);
+}
